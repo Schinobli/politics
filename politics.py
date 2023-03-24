148e8731d7cc2f0500248f023bc330b03f5a5f66
Luke <schinobli1@gmail.com>
2023-03-23 22:04:39 -0300

    ALteracão main 1

--- politics.py
+++ politics.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 from api import Apipol
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='')
 
 # route -> caminho
 # função -> o que quer exibir na página
